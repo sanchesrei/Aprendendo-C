7c848a45b80ca0c5b71fd89efab5091ae97a4865
FelipeSanches <fsanches920@gmail.com>
2025-10-23 17:14:13 -0300

    Adiciona programa inicial de cadastro de usuário"

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main () {
-    printf("Hello, World!\n");
+    int idade, matricula;
+    float altura;
+    char nome[30];
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura (em metros): ");
+    scanf("%f", &altura);   
+
+    printf("digite seu nome: ");
+    scanf("%s", nome);
+
+    printf("Digite sua matricula: ");
+    scanf("%d", &matricula);
+
+    printf("Olá, %s!\n", nome); 
+    printf("Sua idade é %d anos.\n", idade);
+    printf("Sua altura é %.2f metros.\n", altura);  
+    printf("Sua matrícula é %d.\n", matricula);  
+
+
     return 0;
 }
